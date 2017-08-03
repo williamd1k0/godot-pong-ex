54f1725063b99b6fcf5de1c8a39012f1fcc4f179
William Tumeo <tumeowilliam@gmail.com>
2017-08-02 23:00:19 -0300

    Change ball script

---
+++ game/boll.gd
@@ -0,0 +1,65 @@
+extends Sprite
+
+signal out_of_screen(side)
+
+enum {
+	DIR_UP=-1,
+	DIR_DOWN=1,
+	DIR_LEFT=-1,
+	DIR_RIGHT=1
+}
+
+# Member variables
+export(int) var INITIAL_BALL_SPEED = 80
+
+onready var ball_speed = INITIAL_BALL_SPEED
+onready var pads = get_tree().get_nodes_in_group('pad')
+
+var screen_size = OS.get_window_size()
+var pad_size
+
+# Default ball direction
+var direction = Vector2(-1, 0)
+
+func _ready():
+	connect("out_of_screen", self, '_on_out_of_screen')
+	pad_size = pads[0].get_item_rect().size
+	set_process(true)
+
+func _process(delta):
+	# Get ball position
+	var ball_pos = get_pos()
+
+	# Integrate new ball postion
+	ball_pos += direction*ball_speed*delta
+	
+	# Flip when touching roof or floor
+	var dir_y = sign(direction.y)
+	if ((ball_pos.y < 0 and dir_y == DIR_UP)
+		or (ball_pos.y > screen_size.y and dir_y == DIR_DOWN)):
+		direction.y = -direction.y
+		get_node("SamplePlayer2D").play('tap')
+	
+	# Flip, change direction and increase speed when touching pads
+	var dir_x = sign(direction.x)
+	for pad in pads:
+		var pad_rect = Rect2(pad.get_pos() - pad_size*0.5, pad_size)
+		if pad_rect.has_point(ball_pos) and dir_x == sign(pad.get_pos().x - get_pos().x):
+			direction.x = -direction.x
+			ball_speed *= 1.1
+			direction.y = randf()*2.0 - 1
+			direction = direction.normalized()
+			get_node("SamplePlayer2D").play('tap')
+	
+	# Check gameover
+	if ball_pos.x < 0:
+		emit_signal('out_of_screen', DIR_LEFT)
+	elif ball_pos.x > screen_size.x:
+		emit_signal('out_of_screen', DIR_RIGHT)
+	else:
+		set_pos(ball_pos)
+
+func _on_out_of_screen(side):
+	ball_speed = INITIAL_BALL_SPEED
+	direction = Vector2(-1, 0)
+	set_pos(screen_size*0.5)
--- game/pong.gd
+++ game/pong.gd
@@ -1,70 +1,26 @@
-extends Node2D
+extends Node
 
-enum {
-	DIR_UP=-1,
-	DIR_DOWN=1,
-	DIR_LEFT=-1,
-	DIR_RIGHT=1
-}
-
-signal game_over(winner)
-
-# Member variables
-export(int) var INITIAL_BALL_SPEED = 80
-
-onready var ball_speed = INITIAL_BALL_SPEED
-onready var ball = get_node("ball")
-
-var screen_size = OS.get_window_size()
-var pad_size
-
-# Default ball direction
-var direction = Vector2(-1, 0)
+signal p1_update(score)
+signal p2_update(score)
 
 func _ready():
-	connect("game_over", self, '_on_game_over')
-	# TODO: use groups
-	pad_size = get_tree().get_nodes_in_group('pad')[0].get_item_rect().size
-	set_process(true)
+	Score.connect('score_update', self, '_on_score_update')
+	set_process_input(true)
 
-func _process(delta):
-	# Get ball position and pad rectangles
-	var ball_pos = ball.get_pos()
-	var left_rect = Rect2(get_node("left").get_pos() - pad_size*0.5, pad_size)
-	var right_rect = Rect2(get_node("right").get_pos() - pad_size*0.5, pad_size)
-	
-	# Integrate new ball postion
-	ball_pos += direction*ball_speed*delta
-	
-	# Flip when touching roof or floor
-	var dir_y = sign(direction.y)
-	if ((ball_pos.y < 0 and dir_y == DIR_UP)
-		or (ball_pos.y > screen_size.y and dir_y == DIR_DOWN)):
-		direction.y = -direction.y
-		ball.get_node("SamplePlayer2D").play('tap')
-	
-	# Flip, change direction and increase speed when touching pads
-	var dir_x = sign(direction.x)
-	if ((left_rect.has_point(ball_pos) and dir_x == DIR_LEFT)
-		or (right_rect.has_point(ball_pos) and dir_x == DIR_RIGHT)):
-		direction.x = -direction.x
-		ball_speed *= 1.1
-		direction.y = randf()*2.0 - 1
-		direction = direction.normalized()
-		ball.get_node("SamplePlayer2D").play('tap')
-	
-	# Check gameover
-	if ball_pos.x < 0:
-		emit_signal('game_over', 2)
-	elif ball_pos.x > screen_size.x:
-		emit_signal('game_over', 1)
+func _input(event):
+	if event.is_action_pressed("ui_select"):
+		get_tree().set_pause(not get_tree().is_paused())
+
+func _on_score_update(player, score):
+	if player == 1:
+		emit_signal('p1_update', str(score))
 	else:
-		ball.set_pos(ball_pos)
+		emit_signal('p2_update', str(score))
 
-func _on_game_over(winner):
-	ball_speed = INITIAL_BALL_SPEED
-	direction = Vector2(-1, 0)
-	ball.set_pos(screen_size*0.5)
-	Score.add_score(winner)
+func _on_ball_out_of_screen(side):
+	if side == -1:
+		Score.add_score(2)
+	else:
+		Score.add_score(1)
 	get_node("SamplePlayer").play('score')
 	get_tree().call_group(SceneTree.GROUP_CALL_DEFAULT, 'pad', 'reset')
